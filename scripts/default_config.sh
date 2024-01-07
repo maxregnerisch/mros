471e97c881c9e44d1863bb9bf9b56e37e196b052
maxregnerisch <113422283+maxregnerisch@users.noreply.github.com>
2024-01-07 08:47:43 +0100

    Update default_config.sh

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -5,27 +5,27 @@
 # Usage: Copy this file to config.sh and make changes there.  Keep this file (default_config.sh) as-is
 #   so that subsequent changes can be easily merged from upstream.  Keep all customiations in config.sh
 
-# The version of Ubuntu to generate.  Successfully tested: bionic, cosmic, disco, eoan, focal, groovy, jammy
-# See https://wiki.ubuntu.com/DevelopmentCodeNames for details
-export TARGET_UBUNTU_VERSION="jammy"
+# The version of BSD to generate.  Successfully tested: FreeBSD 13.0, OpenBSD 6.9, NetBSD 9.2
+# See the respective BSD websites for details
+export TARGET_BSD_VERSION="FreeBSD 13.0"
 
-# The Ubuntu Mirror URL. It's better to change for faster download.
-# More mirrors see: https://launchpad.net/ubuntu/+archivemirrors
-export TARGET_UBUNTU_MIRROR="http://us.archive.ubuntu.com/ubuntu/"
+# The BSD Mirror URL. It's better to change for faster download.
+# More mirrors see: https://www.freebsd.org/doc/handbook/mirrors.html
+export TARGET_BSD_MIRROR="https://ftp.freebsd.org/pub/FreeBSD/releases/amd64/13.0-RELEASE/"
 
-# The packaged version of the Linux kernel to install on target image.
-# See https://wiki.ubuntu.com/Kernel/LTSEnablementStack for details
-export TARGET_KERNEL_PACKAGE="linux-generic"
+# The packaged version of the BSD kernel to install on target image.
+# See the respective BSD websites for details
+export TARGET_KERNEL_PACKAGE="GENERIC"
 
 # The file (no extension) of the ISO containing the generated disk image,
 # the volume id, and the hostname of the live environment are set from this name.
-export TARGET_NAME="ubuntu-from-scratch"
+export TARGET_NAME="bsd-from-scratch"
 
 # The text label shown in GRUB for booting into the live environment
-export GRUB_LIVEBOOT_LABEL="Try Ubuntu FS without installing"
+export GRUB_LIVEBOOT_LABEL="Try BSD FS without installing"
 
 # The text label shown in GRUB for starting installation
-export GRUB_INSTALL_LABEL="Install Ubuntu FS"
+export GRUB_INSTALL_LABEL="Install BSD FS"
 
 # Packages to be removed from the target system after installation completes succesfully
 export TARGET_PACKAGE_REMOVE="
@@ -40,23 +40,21 @@ export TARGET_PACKAGE_REMOVE="
 # present on the installed system.
 function customize_image() {
     # install graphics and desktop
-    apt-get install -y \
-    plymouth-theme-ubuntu-logo \
-    ubuntu-gnome-desktop \
-    ubuntu-gnome-wallpapers
+    pkg install -y \
+    xorg \
+    xfce
 
     # useful tools
-    apt-get install -y \
-    clamav-daemon \
+    pkg install -y \
+    clamav \
     terminator \
-    apt-transport-https \
     curl \
     vim \
     nano \
     less
 
     # purge
-    apt-get purge -y \
+    pkg remove -y \
     transmission-gtk \
     transmission-common \
     gnome-mahjongg \
